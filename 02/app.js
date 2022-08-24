43c9ea205a487d915ccc7802837b36993c6deced
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2022-08-24 10:56:14 +0200

    end task 01

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,16 @@
 console.log('DOM');
+
+const linksWithDataURL = document.querySelectorAll('a[data-url]')
+console.log(linksWithDataURL)
+
+linksWithDataURL.forEach(function (element) {
+    const containWithDataUrl = element.getAttribute('data-url')
+    element.setAttribute(
+    'href',
+    containWithDataUrl
+   )
+})
+
+
+
+
