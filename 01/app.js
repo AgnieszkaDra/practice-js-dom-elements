8d1516a53195175b18bed2e4fd824a7d302a0e89
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2022-08-24 08:39:56 +0200

    end task 01

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,8 @@
 console.log('DOM');
+
+let searchElement = document.querySelector(`.comments__item.comments__item--newest`)
+
+
+let dataInfoElements = searchElement.querySelectorAll(`[data-info]`).length
+
+console.log(dataInfoElements)
