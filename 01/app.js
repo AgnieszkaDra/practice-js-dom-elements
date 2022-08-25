efb8a2438cf30bd7fedec41ef95fbd2d693c72ac
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2022-08-25 16:41:24 +0200

    end task 05

--- 01/app.js
+++ 01/app.js
@@ -2,7 +2,6 @@ console.log('DOM');
 
 let searchElement = document.querySelector(`.comments__item.comments__item--newest`)
 
-
 let dataInfoElements = searchElement.querySelectorAll(`[data-info]`).length
 
 console.log(dataInfoElements)
--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,53 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+console.dir(curr)
+
+if (curr) {
+
+  const parentCurr = curr.parentNode
+  console.dir(parentCurr)
+
+  const button = document.createElement('button')
+  button.innerText = 'Usuń z koszyka'
+  parentCurr.appendChild(button)
+
+  const parentCurrChildren = curr.parentElement.children
+  console.dir(parentCurrChildren) // HTMLCollection
+
+  const arrayFromParentCurrChildren = [...parentCurrChildren]
+
+  arrayFromParentCurrChildren.forEach(element => {
+    if (element !== curr) {
+      element.setAttribute('class', 'siblings')
+    }
+  }
+  )
+
+  const parentCurrSibling = curr.parentElement.nextElementSibling
+  console.log(parentCurrSibling)
+  parentCurrSibling.setAttribute('title', 'nextElementSibling')
+
+  const lastArticle = parentCurr.nextElementSibling.nextElementSibling
+
+  const lastButton = lastArticle.children[2]
+  // lastButton = lastArticle.children-1
+  console.log(lastButton)
+
+  const lastP = document.createElement('p')
+  lastP.innerText = 'lorem'
+  lastArticle.insertBefore(lastP, lastButton)
+
+  let newFirstArticle = document.createElement('article')
+  newFirstArticle = parentCurr.cloneNode(true)
+  console.log(newFirstArticle)
+  parentCurr.parentElement.insertBefore(newFirstArticle, parentCurr)
+
+};
+
+
+
+
+
+
+
